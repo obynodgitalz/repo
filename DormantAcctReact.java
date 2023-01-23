8e8ba03ba6c0ac68b795215d676cc8265dc39577
obynodgitalz <54357755+obynodgitalz@users.noreply.github.com>
2023-01-23 13:01:08 +0100

    Selenium - Dormant account reactivation
    test scripts

---
+++ DormantAcctReact.java
@@ -0,0 +1,59 @@
+package MelloTest.MelloTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterTest;
+
+public class DormantAcctReact extends LogIn {
+	
+	@AfterTest
+	
+	@SuppressWarnings("deprecation")
+	public void ReactivateDormAccount () throws InterruptedException {
+	
+	driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div/nav/div[1]/div[1]/a[14]/div")).click();
+	
+	driver.findElement(By.id("mui-component-select-dormantAccountType")).click();
+	driver.findElement(By.xpath("//*[@id=\"menu-dormantAccountType\"]/div[3]/ul/li[3]")).click();
+	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	driver.findElement(By.xpath("/html/body/div/div/div[2]/div[1]/div/div/div[1]/div/form/div[2]/div[2]/div[1]/div/div/div")).click();
+	
+	driver.findElement(By.xpath("/html/body/div[2]/div[3]/ul/li")).click();
+	
+	
+	driver.findElement(By.name("agreeTerms")).click();
+	driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[1]/div/div/div[1]/div/form/div[3]/div/button")).click();
+	driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[1]/div/div/div[2]/div/form/div[2]/div/button")).click();
+	
+	driver.findElement(By.name("phoneNumber")).sendKeys("123445678877");
+	driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[1]/div/div/div[4]/div/form/div[2]/div/button")).click();
+	
+	driver.findElement(By.id("mui-component-select-maritalStatus")).click();
+	driver.findElement(By.xpath("//*[@id=\"menu-maritalStatus\"]/div[3]/ul/li[2]")).click();
+	
+	driver.findElement(By.name("nextKin")).sendKeys("Michael toriola");
+	driver.findElement(By.name("kinFullname")).sendKeys("Michael Abiodun Falola");
+	driver.findElement(By.name("kinPhone")).sendKeys("86767778877568");
+	driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[1]/div/div/div[5]/div/form/div[2]/div/button")).click();
+	
+	Thread.sleep(10000);
+	
+	
+	driver.findElement(By.name("digit1")).sendKeys("0");
+	driver.findElement(By.name("digit2")).sendKeys("0");
+	driver.findElement(By.name("digit3")).sendKeys("0");
+	driver.findElement(By.name("digit4")).sendKeys("0");
+	driver.findElement(By.name("digit5")).sendKeys("0");
+	driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/div/form/button")).click();
+	
+	Thread.sleep(10000);
+	
+	driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/section/div[2]/button")).click();
+	
+	
+	System.out.println("Test was completed Successfully");
+	}
+	
+
+}
